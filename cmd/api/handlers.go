a552f1b774e361d36276b5b4a59ec522183f190c
arshia <arshiarezagholi1212@gmail.com>
2024-11-12 01:07:35 +0330

    added the UserRegister struct and registerUser handler

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,7 +1,27 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"time"
+)
+
+type UserRegister struct {
+	Name      string    `json:"name,omitempty"`
+	LastName  string    `json:"last_name,omitempty"`
+	Username  string    `json:"username"`
+	Password  string    `json:"password"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at,omitempty"`
+	UpdatedAt time.Time `json:"updated_at,omitempty"`
+}
 
 func (app *Config) registerUser(c *fiber.Ctx) error {
+	user := new(UserRegister)
 
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "invalid data ",
+			"error":   err.Error(),
+		})
+	}
 }
