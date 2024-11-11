cd9164030f4b8833b003cf489a026eb5fc2e0084
arshia <arshiarezagholi1212@gmail.com>
2024-11-11 14:32:54 +0330

    added the main func and added server of the fiber

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"log"
+)
+
+const webPort = 8080
+
+type Config struct{}
+
+func main() {
+	server := fiber.New()
+
+	err := server.Listen(fmt.Sprintf(":%v", webPort))
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
