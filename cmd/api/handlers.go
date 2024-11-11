a86f1b66f60a526ed24004969614baf350d068ac
arshia <arshiarezagholi1212@gmail.com>
2024-11-12 01:32:19 +0330

    completed the register user handler

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"github.com/gofiber/fiber/v2"
+	"log"
 	"time"
 )
+
+const dbTimeout = time.Second * 3
 
 type UserRegister struct {
 	Name      string    `json:"name,omitempty"`
@@ -19,9 +23,31 @@ func (app *Config) registerUser(c *fiber.Ctx) error {
 	user := new(UserRegister)
 
 	if err := c.BodyParser(user); err != nil {
+		log.Println(err.Error())
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
 			"message": "invalid data ",
 			"error":   err.Error(),
 		})
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), dbTimeout)
+	defer cancel()
+
+	newUser, err := app.client.User.
+		Create().
+		SetName(user.Name).
+		SetLastName(user.LastName).
+		SetUsername(user.Username).
+		SetPassword(user.Password).
+		SetEmail(user.Email).
+		Save(ctx)
+	if err != nil {
+		log.Println(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to create user",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(newUser)
+
 }
