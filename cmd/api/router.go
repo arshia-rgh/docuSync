6d2f2de47824ef233f432398998c5dc4bbb458f5
arshia <arshiarezagholi1212@gmail.com>
2024-11-13 19:02:24 +0330

    added the login handler to the router

--- cmd/api/router.go
+++ cmd/api/router.go
@@ -4,6 +4,7 @@ import "github.com/gofiber/fiber/v2"
 
 func (app *Config) registerPublicRouter(server fiber.Router) {
 	server.Post("/register", app.registerUser)
+	server.Post("/login", app.loginUser)
 }
 
 func (app *Config) registerProtectedRouter(server fiber.Router) {
