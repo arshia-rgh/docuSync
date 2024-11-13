010adb5ff66c7c84d78455cb8f3fc6262bdbbe01
arshia <arshiarezagholi1212@gmail.com>
2024-11-13 18:36:05 +0330

    completed the Generate jwt token function

--- utils/jwt.go
+++ utils/jwt.go
@@ -1,7 +1,20 @@
 package utils
 
-func GenerateToken(userID int) (string, error) {
+import (
+	"github.com/golang-jwt/jwt/v5"
+	"os"
+	"time"
+)
 
+var secretKey = os.Getenv("SECRET_KEY")
+
+func GenerateToken(userID int) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": userID,
+		"exp": time.Now().Add(time.Hour * 3).Unix(),
+	})
+
+	return token.SignedString([]byte(secretKey))
 }
 
 func VerifyToken(token string) (int, error) {
