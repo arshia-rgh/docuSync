32fca596b95c573a9dfe19efa06405d5d10724c9
arshia <arshiarezagholi1212@gmail.com>
2024-11-13 18:57:12 +0330

    completed the loginUser handler

--- cmd/api/handlers.go
+++ cmd/api/handlers.go
@@ -45,6 +45,26 @@ func (app *Config) registerUser(c *fiber.Ctx) error {
 
 }
 
+// loginUser uses the UserLogin schema
 func (app *Config) loginUser(c *fiber.Ctx) error {
+	var user UserLogin
 
+	if err := c.BodyParser(user); err != nil {
+		log.Println(err.Error())
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": "invalid data",
+			"error":   err.Error(),
+		})
+	}
+	token, err := utils.GenerateToken(user.Username)
+	if err != nil {
+		log.Println(err.Error())
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "failed to generate token",
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"code": token,
+	})
 }
--- utils/jwt.go
+++ utils/jwt.go
@@ -9,16 +9,16 @@ import (
 
 var secretKey = os.Getenv("SECRET_KEY")
 
-func GenerateToken(userID int) (string, error) {
+func GenerateToken(userName string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
-		"sub": userID,
+		"sub": userName,
 		"exp": time.Now().Add(time.Hour * 3).Unix(),
 	})
 
 	return token.SignedString([]byte(secretKey))
 }
 
-func VerifyToken(token string) (int, error) {
+func VerifyToken(token string) (string, error) {
 	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (any, error) {
 		_, ok := token.Method.(*jwt.SigningMethodHMAC)
 		if !ok {
@@ -27,19 +27,19 @@ func VerifyToken(token string) (int, error) {
 		return []byte(secretKey), nil
 	})
 	if err != nil {
-		return 0, err
+		return "", err
 	}
 
 	tokenIsValid := parsedToken.Valid
 	if !tokenIsValid {
-		return 0, errors.New("invalid token")
+		return "", errors.New("invalid token")
 	}
 
 	claims, ok := parsedToken.Claims.(jwt.MapClaims)
 	if !ok {
-		return 0, errors.New("invalid claims")
+		return "", errors.New("invalid claims")
 	}
 
-	userID := int(claims["sub"].(float64))
-	return userID, nil
+	userName := claims["sub"].(string)
+	return userName, nil
 }
