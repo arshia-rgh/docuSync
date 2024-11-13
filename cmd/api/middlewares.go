ab7139d7e6912fccda89edf1537fe1fe5815d116
arshia <arshiarezagholi1212@gmail.com>
2024-11-13 16:45:04 +0330

    added request logger midleware

---
+++ cmd/api/middlewares.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"github.com/gofiber/fiber/v2"
+	"time"
+)
+
+func (app *Config) requestLogger(c *fiber.Ctx) error {
+	start := time.Now()
+
+	// Process request
+	err := c.Next()
+
+	app.logger.Log("info", "Request received", map[string]any{
+		"Method":   c.Method(),
+		"URL":      c.OriginalURL(),
+		"Status":   c.Response().StatusCode(),
+		"Duration": time.Since(start),
+	})
+
+	return err
+}
